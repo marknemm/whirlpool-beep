b3f58df8dc9800ddcc6864b52a21c9a4afcff3eb
Mark Nemmer <marknemmer@gmail.com>
2024-06-22 18:23:37 -0400

    Start dealing with secret data and encoding/decoding keypairs.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-console.log('Hello, TypeScript');
+import { exec } from 'child_process';
+import 'dotenv/config';
+import { decodeBase58 } from './util/encode';
+import { writeFileSync } from 'fs';
+
+function main() {
+  if (!process.env.WALLET_PRIVATE_KEY) {
+    throw new Error('WALLET_PRIVATE_KEY is not set');
+  }
+  if (!process.env.WALLET_ADDRESS) {
+    throw new Error('WALLET_ADDRESS is not set');
+  }
+  const walletJsonFile = process.env.WALLET_JSON_FILE || 'wallet.json';
+
+  const privateKeyBytes = decodeBase58(process.env.WALLET_PRIVATE_KEY);
+  writeFileSync(walletJsonFile, `[${privateKeyBytes.toString()}]`);
+
+  exec(`solana address -k ${walletJsonFile}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing command: ${error.message}`);
+      return;
+    }
+
+    if (stdout) {
+      console.log(stdout);
+      console.log(`Wallet JSON file created successfully: ${stdout.trim() === process.env.WALLET_ADDRESS?.trim()}`);
+    }
+
+    if (stderr) {
+      console.error(stderr);
+    }
+  });
+}
+
+main();
