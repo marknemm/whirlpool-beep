a1a92893b8e80aa98b94df1159fc80ce81f64b38
Mark Nemmer <marknemmer@gmail.com>
2024-06-23 12:42:45 -0400

    Improve eslint config.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,16 @@
-import pluginJs from '@eslint/js';
-import globals from 'globals';
+// @ts-check
+
+import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default [
-  {
-    files: ['src/**/*.{js,mjs,cjs,ts}'],
-  },
-  {
-    languageOptions: {
-      globals: globals.browser,
-    }
-  },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-];
+export default tseslint.config({
+  files: ['src/**/*.{js,mjs,cjs,ts}'],
+  extends: [
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+  ],
+  rules: {
+    'quotes': ['warn', 'single'],
+  }
+});
+
